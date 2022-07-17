8928cb5e22f26c704195d1c8bfdf4a08146fe447
Igor Pravdin <pravdin_i_igor@ukr.net>
2022-07-17 18:18:08 +0300

    2.0.0

--- src/schema.js
+++ src/schema.js
@@ -26,7 +26,7 @@ Schema.schema = (name, {req = [], opt = []}) => {
     if (!Array.isArray(req) || !Array.isArray(opt)) {
         throw new TypeError(`Schema's 'req' or 'opt' is not an array.`);
     }
-    [...req, ...opt].forEach(s => {
+    [...opt, ...req].forEach(s => {
         if (!(s instanceof Spec) && !(s instanceof Schema)) {
             throw new TypeError(`Invalid spec or schema passed.`);
         }
@@ -36,26 +36,48 @@ Schema.schema = (name, {req = [], opt = []}) => {
 
 Schema.check = (schema, input) => {
     if (!(schema instanceof Schema)) {
-        throw new TypeError(`Invalid 'schema' passed. Expected instance of 'Schema'.`);
+        throw new TypeError(`Invalid schema passed. Expected instance of 'Schema'.`);
     }
     const state = states.get(schema);
-    const required = state.req.map(spec => states.get(spec).name); //todo
     state.conformed = undefined;
     state.errors = [];
-    state.input = {...input};
-    states.set(schema, state);
-    required.forEach(field => {
-        if (input[field] === undefined) {
+    state.input = input;
+    state.req.map(s => states.get(s).name).forEach(field => {
+        if (state.input[field] === undefined) {
             state.errors.push({
                 schema: state.name,
-                input,
-                message: `Required field '${field}' is missing.`
+                input: state.input,
+                message: `Required field '${field}' is missing for schema '${state.name}'.`
             });
+        }
+    });
+    [...state.req, ...state.opt].forEach(s => {
+        let field = states.get(s).name;
+        if(state.input[field] !== undefined) {
+            switch (true) {
+                case (s instanceof Spec):
+                    let result = Spec.check(s, state.input[field]);
+                    if (result.isValid()) {
+                        state.input[field] = result.conform();
+                        state.conformed = state.input;
+                    } else {
+                        state.errors.push({schema: state.name, ...result.explain()});
+                    }
+                    break;
+                case (s instanceof Schema):
+                    console.log('schema'); //todo
+                    break;
+            }
         }
     });
     states.set(schema, state);
     return {...state};
 }
+
+/*** ***/
+
+
+
 
 let specFirstName = Spec.spec('firstName').trim().isLength({min: 3, max: 32})
 let specSecondName = Spec.spec('lastName').trim().isLength({min: 3, max: 32})
@@ -63,9 +85,9 @@ let specAge = Spec.spec('age').trim().isInt({min: 0}).toInt()
 
 let schm = Schema.schema('foo', {req: [specFirstName, specSecondName, specAge]})
 let res = Schema.check(schm, {
-    firstName: "Toad",
+    firstName: "  Toad",
     lastName: "Person",
-    age: 35
+    age: "35  "
 })
 
 console.log(res)
