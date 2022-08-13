3d61167755d6a44d5f1691aa59177eba6560b246
Igor Pravdin <pravdin_i_igor@ukr.net>
2022-08-13 21:21:38 +0300

    2.0.0 - TS

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import validator from 'validator';
+
+///////////////
+// CONSTANTS //
+///////////////
+
+export const VALID: symbol = Symbol("VALID");
+export const INVALID: symbol = Symbol("INVALID");
+
