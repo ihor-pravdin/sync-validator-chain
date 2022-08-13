71d1d36a9c6f91992a3ce9e92acfce0d335d2d87
Igor Pravdin <pravdin_i_igor@ukr.net>
2022-08-14 01:59:40 +0300

    2.0.0-TS

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 "use strict";
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a, _Schema_check;
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.INVALID = exports.VALID = void 0;
-const validator_1 = __importDefault(require("validator"));
+exports.Schema = exports.Spec = exports.INVALID = exports.VALID = void 0;
+const validator_1 = __importDefault(require("validator")); // validator.js
 /////////////////
 //             //
 //  CONSTANTS  //
@@ -12,8 +18,22 @@ const validator_1 = __importDefault(require("validator"));
 /////////////////
 exports.VALID = Symbol("VALID");
 exports.INVALID = Symbol("INVALID");
-const states = new WeakMap();
+//////////////
+//          //
+//  STATES  //
+//          //
+//////////////
+const states = new WeakMap(); // states of validator chains
+////////////
+//        //
+//  SPEC  //
+//        //
+////////////
 class Spec {
+    /**
+     * Spec Constructor
+     * @param name
+     */
     constructor(name) {
         const state = { name, fns: new Map() };
         states.set(this, state);
@@ -24,9 +44,20 @@ class Spec {
             };
         });
     }
+    /**
+     * Factory method. Creates new Spec validation chain.
+     * @param name
+     * @returns
+     */
     static spec(name) {
         return new Spec(name);
     }
+    /**
+     * Checks input value according to validation chain rules.
+     * @param spec
+     * @param input
+     * @returns
+     */
     static check(spec, input) {
         const state = states.get(spec);
         state.input = '' + input;
@@ -52,11 +83,144 @@ class Spec {
                 }
             });
         }
-        return state;
+        return new SpecValidationResult(state);
     }
 }
-let foo = Spec.spec('foo').isInt({ min: 0 }).toInt();
-//console.log(foo)
-// console.log(foo.isInt)
-// console.log(foo.isInt({ min: 0 }).toInt())
-console.log(Spec.check(foo, '10'));
+exports.Spec = Spec;
+//////////////////////////////
+//                          //
+//  SPEC VALIDATION RESULT  //
+//                          //
+//////////////////////////////
+class SpecValidationResult {
+    /**
+     * Spec Validation Result Constructor
+     * @param state
+     */
+    constructor(state) {
+        const { name: spec, input, error, conformed } = state;
+        const rules = [...state.fns.keys()];
+        this.isValid = () => error === undefined;
+        this.conform = () => this.isValid() ? conformed || input : exports.INVALID;
+        this.explain = () => this.isValid() ? exports.VALID : { spec, message: error, rules };
+    }
+}
+//////////////
+//          //
+//  SCHEMA  //
+//          //
+//////////////
+class Schema {
+    /**
+     * Schema Constructor
+     * @param name
+     * @param fields
+     */
+    constructor(name, fields) {
+        const state = {
+            name,
+            errors: [],
+            req: [...fields.req],
+            opt: [...fields.opt]
+        };
+        states.set(this, state);
+    }
+    /**
+     * Factory method. Creates new Schema validator.
+     * @param name
+     * @param input
+     * @returns
+     */
+    static schema(name, input) {
+        return new Schema(name, input);
+    }
+    /**
+     * Checks input value according to validation schema.
+     * @param schema
+     * @param input
+     */
+    static check(schema, input = {}) {
+        const state = states.get(schema);
+        state.input = input;
+        state.conformed = undefined;
+        state.errors = [];
+        let field;
+        state.req.forEach(validator => {
+            field = states.get(validator).name;
+            if (state.input[field] !== undefined) {
+                __classPrivateFieldGet(Schema, _a, "m", _Schema_check).call(Schema, state, validator);
+            }
+            else {
+                state.errors = [...state.errors, {
+                        schema: state.name,
+                        message: `Required field '${field}' is missing for schema '${state.name}'.`
+                    }];
+            }
+        });
+        state.opt.forEach(validator => {
+            field = states.get(validator).name;
+            if (state.input[field] !== undefined) {
+                __classPrivateFieldGet(Schema, _a, "m", _Schema_check).call(Schema, state, validator);
+            }
+        });
+        return new SchemaValidationResult(state);
+    }
+}
+exports.Schema = Schema;
+_a = Schema, _Schema_check = function _Schema_check(state, validator) {
+    const field = states.get(validator).name;
+    const input = state.input[field];
+    let result, explanation;
+    switch (validator.constructor.name) {
+        case "Spec":
+            result = Spec.check(validator, input);
+            if (result.isValid()) {
+                state.input[field] = result.conform();
+                state.conformed = state.input;
+            }
+            else {
+                explanation = result.explain();
+                state.errors = [...state.errors, {
+                        schema: state.name,
+                        spec: explanation.spec,
+                        message: `schema: ${state.name}, ` + explanation.message,
+                        rules: explanation.rules
+                    }];
+            }
+            break;
+        case "Schema":
+            result = Schema.check(validator, input);
+            if (!result.isValid()) {
+                state.errors.push(...result.explain());
+            }
+            break;
+    }
+};
+////////////////////////////////
+//                            //
+//  SCHEMA VALIDATION RESULT  //
+//                            //
+////////////////////////////////
+class SchemaValidationResult {
+    /**
+     * Schema Validation Result Constructor
+     * @param state
+     */
+    constructor(state) {
+        const { input, errors, conformed } = state;
+        this.isValid = () => errors.length === 0;
+        this.conform = () => this.isValid() ? conformed || input : exports.INVALID;
+        this.explain = () => this.isValid() ? exports.VALID : errors;
+    }
+}
+///
+let foo = Spec.spec('foo').trim().isInt({ min: 5 }).toInt();
+let bar = Spec.spec('bar').trim().isInt({ min: 5 }).toInt();
+console.log('typeof', typeof foo);
+console.log('constructor', foo.constructor.name);
+console.log('isValid', Spec.check(foo, ' 10').isValid());
+console.log('conform', Spec.check(foo, ' 10').conform());
+console.log('explain', Spec.check(foo, ' 10').explain());
+console.log('isValid', Spec.check(bar, ' 1').isValid());
+console.log('conform', Spec.check(bar, ' 1').conform());
+console.log('explain', Spec.check(bar, ' 1').explain());
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import validator from 'validator'; //validator.js
+import vjs from 'validator'; // validator.js
 
 /////////////////
 //             //
@@ -9,14 +9,45 @@ import validator from 'validator'; //validator.js
 export const VALID: symbol = Symbol("VALID");
 export const INVALID: symbol = Symbol("INVALID");
 
+//////////////////////////
+//                      //
+//  TYPES & INTERFACES  //
+//                      //
+//////////////////////////
+
+type States = WeakMap<Validators, ValidatorStates>;
+type VJS = keyof typeof vjs;
+type Key<Type> = Type;
+type Value<Type> = (...args: any[]) => Type;
+type Validators = Spec | Schema;
+type ValidatorStates = SpecState | SchemaState;
+
+interface StateInterface {
+    name: string
+}
+
+interface SpecState extends StateInterface {
+    input?: string
+    error?: string
+    conformed?: any
+    fns: Map<Key<VJS>, [Value<any>, any[]]>
+}
+
+interface SchemaState extends StateInterface {
+    input?: any
+    errors: any[],
+    conformed?: any,
+    req: Validators[],
+    opt: Validators[]
+}
+
 //////////////
 //          //
 //  STATES  //
 //          //
 //////////////
 
-type States = WeakMap<Spec, SpecState>;
-const states: States = new WeakMap();
+const states: States = new WeakMap(); // states of validator chains
 
 ////////////
 //        //
@@ -24,47 +55,29 @@ const states: States = new WeakMap();
 //        //
 ////////////
 
-type Validator = keyof typeof validator;
-type Key<Type> = Type;
-type Value<Type> = (...args: any[]) => Type;
-
-// Spec State Interface
-
-interface SpecState {
-    name: string,
-    input?: string,
-    error?: string,
-    conformed?: string,
-    fns: Map<Key<Validator>, [Value<any>, any[]]>
-}
-
-// Spec
-
-class Spec {
+export class Spec {
     /**
-     * Spec constructor.
+     * Spec Constructor
      * @param name 
      */
     protected constructor(name: string) {
         const state: SpecState = { name, fns: new Map() };
         states.set(this, state);
-        (Object.keys(validator) as Key<Validator>[]).forEach((method): void => {
+        (Object.keys(vjs) as Key<VJS>[]).forEach((method): void => {
             Spec.prototype[method] = (...args: any[]): Spec => {
-                state.fns.set(method, [(validator[(method as Key<Validator>)] as Value<any>).bind(validator), args]);
+                state.fns.set(method, [(vjs[(method as Key<VJS>)] as Value<any>).bind(vjs), args]);
                 return this;
             }
         });
     }
 
     /**
-     * Validation methods.
+     * Coppied validation rules from validator.js
      */
     [index: string]: Value<Spec>
 
-    // STATIC
-
     /**
-     * Creates new Spec instance.
+     * Factory method. Creates new Spec validation chain.
      * @param name 
      * @returns 
      */
@@ -73,12 +86,12 @@ class Spec {
     }
 
     /**
-     * Check validation chain.
+     * Checks input value according to validation chain rules.
      * @param spec 
      * @param input 
      * @returns 
      */
-    public static check(spec: Spec, input: any): SpecState {
+    public static check(spec: Spec, input: any): SpecValidationResult {
         const state: SpecState = states.get(spec) as SpecState;
         state.input = '' + input;
         state.conformed = undefined;
@@ -86,7 +99,7 @@ class Spec {
         if (state.fns.size === 0) {
             state.conformed = state.input;
         } else {
-            state.fns.forEach((tuple: any[], method: Key<Validator>): void => {
+            state.fns.forEach((tuple: any[], method: Key<VJS>): void => {
                 let fn: Value<any> = tuple[0];
                 let args: any[] = tuple[1];
                 let { name, input, error, conformed } = state;
@@ -101,15 +114,157 @@ class Spec {
                 }
             });
         }
-        return state;
+        return new SpecValidationResult(state);
     }
 }
 
+//////////////////////////////
+//                          //
+//  SPEC VALIDATION RESULT  //
+//                          //
+//////////////////////////////
 
+class SpecValidationResult {
+    isValid: () => boolean
+    conform: () => any
+    explain: () => any
+    /**
+     * Spec Validation Result Constructor
+     * @param state 
+     */
+    constructor(state: SpecState) {
+        const { name: spec, input, error, conformed } = state;
+        const rules = [...state.fns.keys()];
+        this.isValid = () => error === undefined;
+        this.conform = () => this.isValid() ? conformed || input : INVALID;
+        this.explain = () => this.isValid() ? VALID : { spec, message: error, rules };
+    }
+}
 
-let foo = Spec.spec('foo').isInt({ min: 0 }).toInt()
-//console.log(foo)
-// console.log(foo.isInt)
-// console.log(foo.isInt({ min: 0 }).toInt())
-console.log(Spec.check(foo, '10'))
+//////////////
+//          //
+//  SCHEMA  //
+//          //
+//////////////
 
+export class Schema {
+    /**
+     * Schema Constructor
+     * @param name 
+     * @param fields 
+     */
+    protected constructor(name: string, fields: { req: Validators[], opt: Validators[] }) {
+        const state: SchemaState = {
+            name,
+            errors: [],
+            req: [...fields.req],
+            opt: [...fields.opt]
+        };
+        states.set(this, state);
+    }
+
+    /**
+     * Factory method. Creates new Schema validator.
+     * @param name 
+     * @param input 
+     * @returns 
+     */
+    public static schema(name: string, input: any): Schema {
+        return new Schema(name, input);
+    }
+
+    /**
+     * Checks input value according to validation schema.
+     * @param schema 
+     * @param input 
+     */
+    public static check(schema: Schema, input: any = {}): SchemaValidationResult {
+        const state: SchemaState = states.get(schema) as SchemaState;
+        state.input = input;
+        state.conformed = undefined;
+        state.errors = [];
+        let field;
+        state.req.forEach(validator => {
+            field = (states.get(validator) as ValidatorStates).name;
+            if (state.input[field] !== undefined) {
+                Schema.#check(state, validator);
+            } else {
+                state.errors = [...state.errors, {
+                    schema: state.name,
+                    message: `Required field '${field}' is missing for schema '${state.name}'.`
+                }];
+            }
+        });
+        state.opt.forEach(validator => {
+            field = (states.get(validator) as ValidatorStates).name;
+            if (state.input[field] !== undefined) {
+                Schema.#check(state, validator);
+            }
+        });
+        return new SchemaValidationResult(state);
+    }
+
+    static #check(state: SchemaState, validator: Validators) {
+        const field = (states.get(validator) as StateInterface).name;
+        const input = state.input[field];
+        let result, explanation;
+        switch (validator.constructor.name) {
+            case "Spec":
+                result = Spec.check(validator as Spec, input);
+                if (result.isValid()) {
+                    state.input[field] = result.conform();
+                    state.conformed = state.input;
+                } else {
+                    explanation = result.explain();
+                    state.errors = [...state.errors, {
+                        schema: state.name,
+                        spec: explanation.spec,
+                        message: `schema: ${state.name}, ` + explanation.message,
+                        rules: explanation.rules
+                    }];
+                }
+                break;
+            case "Schema":
+                result = Schema.check(validator as Schema, input);
+                if (!result.isValid()) {
+                    state.errors.push(...result.explain());
+                }
+                break;
+        }
+    }
+}
+
+////////////////////////////////
+//                            //
+//  SCHEMA VALIDATION RESULT  //
+//                            //
+////////////////////////////////
+
+class SchemaValidationResult {
+    isValid: () => boolean
+    conform: () => any
+    explain: () => any
+    /**
+     * Schema Validation Result Constructor
+     * @param state 
+     */
+    constructor(state: SchemaState) {
+        const { input, errors, conformed } = state;
+        this.isValid = () => errors.length === 0;
+        this.conform = () => this.isValid() ? conformed || input : INVALID;
+        this.explain = () => this.isValid() ? VALID : errors;
+    }
+}
+
+///
+
+let foo = Spec.spec('foo').trim().isInt({ min: 5 }).toInt()
+let bar = Spec.spec('bar').trim().isInt({ min: 5 }).toInt()
+console.log('typeof', typeof foo)
+console.log('constructor', foo.constructor.name)
+console.log('isValid', Spec.check(foo, ' 10').isValid())
+console.log('conform', Spec.check(foo, ' 10').conform())
+console.log('explain', Spec.check(foo, ' 10').explain())
+console.log('isValid', Spec.check(bar, ' 1').isValid())
+console.log('conform', Spec.check(bar, ' 1').conform())
+console.log('explain', Spec.check(bar, ' 1').explain())
